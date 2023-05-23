e2d0b5155c3546be79f9739ac34ad31a521390c3
Fitra Ilyasa <fitrailyasa12@gmail.com>
2023-05-24 00:15:09 +0700

    Update login.php

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -4,5 +4,31 @@ Login
 <?= $this->endSection() ?>
 
 <?= $this->section('content') ?>
-<h1>Login</h1>
+<div class="container">
+    <div class="row justify-content-md-center">
+        <div class="col-5">
+
+            <h2>Login</h2>
+
+            <?php if (session()->getFlashdata('msg')) : ?>
+                <div class="alert alert-warning">
+                    <?= session()->getFlashdata('msg') ?>
+                </div>
+            <?php endif; ?>
+            <form action="#" method="post">
+                <div class="form-group mb-3">
+                    <input type="email" name="email" placeholder="Email" value="" class="form-control">
+                </div>
+                <div class="form-group mb-3">
+                    <input type="password" name="password" placeholder="Password" class="form-control">
+                </div>
+
+                <div class="d-grid">
+                    <button type="submit" class="btn btn-primary">Masuk</button>
+                </div>
+            </form>
+        </div>
+
+    </div>
+</div>
 <?= $this->endSection() ?>
