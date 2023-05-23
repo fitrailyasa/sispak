490b98f5cf178a77b4887a91454e66a6c56f0e9d
Fitra Ilyasa <fitrailyasa12@gmail.com>
2023-05-24 05:39:16 +0700

    update

--- app/Database/Seeds/GejalaSeeder.php
+++ app/Database/Seeds/GejalaSeeder.php
@@ -3,23 +3,57 @@
 namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
+use App\Models\GejalaModel;
 
 class GejalaSeeder extends Seeder
 {
     public function run()
     {
-        $faker = \Faker\Factory::create();
+        $gejalaData = [
+            ['kode_gejala' => 'G1', 'nama_gejala' => 'Tombol hidup tapi tidak ada gambar yang ditampilkan terpantau'],
+            ['kode_gejala' => 'G2', 'nama_gejala' => 'Terdapat garis horizontal / vertikal di monitor'],
+            ['kode_gejala' => 'G3', 'nama_gejala' => 'Tidak ada tampilan awal bios'],
+            ['kode_gejala' => 'G4', 'nama_gejala' => 'Muncul pesan Error pada bios (pesan selalu berbeda tergantung kondisi tertentu)'],
+            ['kode_gejala' => 'G5', 'nama_gejala' => 'Alarm bios berbunyi'],
+            ['kode_gejala' => 'G6', 'nama_gejala' => 'Terdengar suara aneh di HDD'],
+            ['kode_gejala' => 'G7', 'nama_gejala' => 'Sering hang/crash saat menjalankan aplikasi'],
+            ['kode_gejala' => 'G8', 'nama_gejala' => 'Selalu pindai disk saat boot'],
+            ['kode_gejala' => 'G9', 'nama_gejala' => 'Muncul pesan error saat menjalankan aplikasi'],
+            ['kode_gejala' => 'G10', 'nama_gejala' => 'Informasi driver perangkat tidak terdeteksi di manajer perangkat, meskipun driver telah diinstal'],
+            ['kode_gejala' => 'G11', 'nama_gejala' => 'Tiba-tiba OS restart otomatis'],
+            ['kode_gejala' => 'G12', 'nama_gejala' => 'Keluarnya blue screen pada OS (pesan selalu berbeda tergantung kondisi tertentu)'],
+            ['kode_gejala' => 'G13', 'nama_gejala' => 'Suara tetap keluar meskipun driver perangkat dan pengaturan telah dilakukan sesuai dengan instruksi'],
+            ['kode_gejala' => 'G14', 'nama_gejala' => 'Muncul pesan error saat menjalankan aplikasi audio'],
+            ['kode_gejala' => 'G15', 'nama_gejala' => 'Muncul pesan kesalahan saat pertama kali OS dimuat dari HDD'],
+            ['kode_gejala' => 'G16', 'nama_gejala' => 'Tidak ada tanda-tanda sebagain/seluruh alat berfungsi (semua kipas pendingin tidak berputar)'],
+            ['kode_gejala' => 'G17', 'nama_gejala' => 'Sering mati mendadak tanpa sebab'],
+            ['kode_gejala' => 'G18', 'nama_gejala' => 'Muncul pesan di windows, bahwa windows virtual memory kekurangan'],
+            ['kode_gejala' => 'G19', 'nama_gejala' => 'Aplikasi berjalan dengan lambat, respons yang lambat terhadap input'],
+            ['kode_gejala' => 'G20', 'nama_gejala' => 'Performa grafis sangat berat (biasanya di internet terbuka dan manipulasi gambar)'],
+            ['kode_gejala' => 'G21', 'nama_gejala' => 'Perangkat tidak terdeteksi di bios'],
+            ['kode_gejala' => 'G22', 'nama_gejala' => 'Deteksi informasi salah di bios'],
+            ['kode_gejala' => 'G23', 'nama_gejala' => 'Bagian dari perangkatnya berfungsi'],
+            ['kode_gejala' => 'G24', 'nama_gejala' => 'Sebagian / seluruh karakter input mati'],
+            ['kode_gejala' => 'G25', 'nama_gejala' => 'Pointer mouse tidak merespon gerakan mouse'],
+            ['kode_gejala' => 'G26', 'nama_gejala' => 'Sebagian / seluruh karakter input mati'],
+            ['kode_gejala' => 'G27', 'nama_gejala' => 'Keluar bip panjang saat laptop dihidupkan'],
+            ['kode_gejala' => 'G28', 'nama_gejala' => 'Dihidupkan agak susah'],
+            ['kode_gejala' => 'G29', 'nama_gejala' => 'Kabel power mengalami goresan'],
+            ['kode_gejala' => 'G30', 'nama_gejala' => 'Tidak ada indikasi daya masuk'],
+            ['kode_gejala' => 'G31', 'nama_gejala' => 'Mati total'],
+            ['kode_gejala' => 'G32', 'nama_gejala' => 'Posisi komputer hidup dan kemudian tiba-tiba layar mati'],
+            ['kode_gejala' => 'G33', 'nama_gejala' => 'Bunyi bip keluar berulang kali'],
+            ['kode_gejala' => 'G34', 'nama_gejala' => 'Lupakan sampai sistem operasi telah di-restart lagi'],
+            ['kode_gejala' => 'G35', 'nama_gejala' => 'Kipas power supply tidak berputar'],
+            ['kode_gejala' => 'G36', 'nama_gejala' => 'Kipas power supply dan kipas prosesor berputar'],
+            ['kode_gejala' => 'G37', 'nama_gejala' => 'CD / DVD / ROM / RW tidak terdeteksi di OS'],
+            ['kode_gejala' => 'G38', 'nama_gejala' => 'CD / DVD / ROM / RW drive yang tidak bisa keluar CD'],
+            ['kode_gejala' => 'G39', 'nama_gejala' => 'Pesan "Kesalahan checksum BIOS ROM - Sistem dihentikan"'],
+            ['kode_gejala' => 'G40', 'nama_gejala' => 'Sistem Operasi Tidak Muncul'],
+            ['kode_gejala' => 'G41', 'nama_gejala' => 'Saat di tekan tombol power tidak menyala'],
+        ];
 
-        for ($i = 1; $i <= 100; $i++) {
-            $data = [
-                'kode_gejala' => 'G' . str_pad($i, 2, '0', STR_PAD_LEFT),
-                'nama_gejala' => 'Gejala ' . str_pad($i, STR_PAD_LEFT),
-                'pertanyaan' => 'Bagaimana Kondisi Gejala ' . str_pad($i, STR_PAD_LEFT) . '?',
-                'bobot' => $faker->numberBetween(1, 10),
-                'created_at' => date('Y-m-d H:i:s')
-            ];
-
-            $this->db->table('gejala')->insert($data);
-        }
+        $model = new GejalaModel();
+        $model->insertBatch($gejalaData);
     }
 }
--- app/Database/Seeds/KerusakanSeeder.php
+++ app/Database/Seeds/KerusakanSeeder.php
@@ -3,22 +3,38 @@
 namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
+use App\Models\KerusakanModel;
 
 class KerusakanSeeder extends Seeder
 {
     public function run()
     {
-        $faker = \Faker\Factory::create();
+        $kerusakanData = [
+            ['kode_kerusakan' => 'K1', 'nama_kerusakan' => 'LCD'],
+            ['kode_kerusakan' => 'K2', 'nama_kerusakan' => 'RAM (Random Access Memory)'],
+            ['kode_kerusakan' => 'K3', 'nama_kerusakan' => 'HDD (Hard Disk Drive)'],
+            ['kode_kerusakan' => 'K4', 'nama_kerusakan' => 'VGA (Video Graphic Accelerator)'],
+            ['kode_kerusakan' => 'K5', 'nama_kerusakan' => 'Sound Card'],
+            ['kode_kerusakan' => 'K6', 'nama_kerusakan' => 'OS Bermasalah'],
+            ['kode_kerusakan' => 'K7', 'nama_kerusakan' => 'Aplikasi Rusak'],
+            ['kode_kerusakan' => 'K8', 'nama_kerusakan' => 'Power Supply'],
+            ['kode_kerusakan' => 'K9', 'nama_kerusakan' => 'Processor'],
+            ['kode_kerusakan' => 'K10', 'nama_kerusakan' => 'Kekurangan Memori'],
+            ['kode_kerusakan' => 'K11', 'nama_kerusakan' => 'Kekurangan Memori VGA'],
+            ['kode_kerusakan' => 'K12', 'nama_kerusakan' => 'Clock Processor Kurang Tinggi'],
+            ['kode_kerusakan' => 'K13', 'nama_kerusakan' => 'Front Panel Error'],
+            ['kode_kerusakan' => 'K14', 'nama_kerusakan' => 'Kekurangan Daya pada Power Supply'],
+            ['kode_kerusakan' => 'K15', 'nama_kerusakan' => 'Perangkat USB Rusak'],
+            ['kode_kerusakan' => 'K16', 'nama_kerusakan' => 'Keyboard Rusak'],
+            ['kode_kerusakan' => 'K17', 'nama_kerusakan' => 'Mouse Rusak'],
+            ['kode_kerusakan' => 'K18', 'nama_kerusakan' => 'Motherboard Rusak'],
+            ['kode_kerusakan' => 'K19', 'nama_kerusakan' => 'Kabel Power'],
+            ['kode_kerusakan' => 'K20', 'nama_kerusakan' => 'Kabel Sata/Ide'],
+            ['kode_kerusakan' => 'K21', 'nama_kerusakan' => 'CD/DVD/ROM/RW'],
+            ['kode_kerusakan' => 'K22', 'nama_kerusakan' => 'Bios Error'],
+        ];
 
-        for ($i = 1; $i <= 100; $i++) {
-            $data = [
-                'kode_kerusakan' => 'K' . str_pad($i, 2, '0', STR_PAD_LEFT),
-                'nama_kerusakan' => 'Kerusakan ' . str_pad($i, STR_PAD_LEFT),
-                'solusi' => 'Solusi ' . str_pad($i, STR_PAD_LEFT),
-                'created_at' => date('Y-m-d H:i:s')
-            ];
-
-            $this->db->table('kerusakan')->insert($data);
-        }
+        $model = new KerusakanModel();
+        $model->insertBatch($kerusakanData);
     }
 }
