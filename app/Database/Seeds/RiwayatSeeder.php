a7c0a6579732f4ae4343572d6c9e3c4d3addd127
Fitra Ilyasa <fitrailyasa12@gmail.com>
2023-05-24 10:24:33 +0700

    update

--- app/Database/Seeds/RiwayatSeeder.php
+++ app/Database/Seeds/RiwayatSeeder.php
@@ -4,27 +4,88 @@ namespace App\Database\Seeds;
 
 use CodeIgniter\Database\Seeder;
 use App\Models\RiwayatModel;
-use Faker\Factory;
 
 class RiwayatSeeder extends Seeder
 {
     public function run()
     {
-        $faker = Factory::create();
-
         $riwayatModel = new RiwayatModel();
 
-        for ($i = 0; $i < 10; $i++) {
-            $data = [
-                'token' => $faker->word,
-                'kerusakan_id' => $faker->numberBetween(1, 5),
-                'merk_laptop' => $faker->word,
-                'tipe_laptop' => $faker->word,
-                'created_at' => $faker->dateTimeThisYear()->format('Y-m-d H:i:s'),
-                'updated_at' => $faker->dateTimeThisYear()->format('Y-m-d H:i:s'),
-            ];
+        $data = [
+            [
+                'token' => 'ABC123',
+                'kerusakan_id' => 1,
+                'merk_laptop' => 'Acer',
+                'tipe_laptop' => 'Aspire',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'token' => 'DEF456',
+                'kerusakan_id' => 2,
+                'merk_laptop' => 'Lenovo',
+                'tipe_laptop' => 'ThinkPad',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'token' => 'GHI789',
+                'kerusakan_id' => 3,
+                'merk_laptop' => 'Dell',
+                'tipe_laptop' => 'Latitude',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'token' => 'JKL012',
+                'kerusakan_id' => 4,
+                'merk_laptop' => 'HP',
+                'tipe_laptop' => 'Pavilion',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'token' => 'MNO345',
+                'kerusakan_id' => 5,
+                'merk_laptop' => 'Asus',
+                'tipe_laptop' => 'ZenBook',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'token' => 'PQR678',
+                'kerusakan_id' => 1,
+                'merk_laptop' => 'Acer',
+                'tipe_laptop' => 'Predator',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'token' => 'STU901',
+                'kerusakan_id' => 2,
+                'merk_laptop' => 'Lenovo',
+                'tipe_laptop' => 'IdeaPad',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'token' => 'VWX234',
+                'kerusakan_id' => 3,
+                'merk_laptop' => 'Dell',
+                'tipe_laptop' => 'XPS',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+            [
+                'token' => 'YZA567',
+                'kerusakan_id' => 4,
+                'merk_laptop' => 'HP',
+                'tipe_laptop' => 'EliteBook',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ],
+        ];
 
-            $riwayatModel->insert($data);
-        }
+        $riwayatModel->insertBatch($data);
     }
 }
